f1117d940a5a4e2db399cc4de2a7d06fb97b2554
maks <maksaturekeshov@gmail.com>
2018-10-28 00:36:20 +0600

    change env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   host: 'localhost:3000',
-  hostUrl: 'http://localhost:5000/api/',
+  hostUrl: 'http://localhost:3000/api/',
   hostEndPath: ''
 };
